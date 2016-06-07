22f70d91435c30feb1f07e86af286145f7cd3ebc
Kevin Quing <KevinQ@iqmetrix.com>
2016-06-08 02:47:21 +0800

    Added: DTO project Office DTO

---
+++ BeerTapV2/BeerTapV2.DTO/OfficeDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerTapV2.DTO
+{
+    public class OfficeResourceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class OfficeEntityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
