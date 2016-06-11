ae6e1845a4af2446df6e91e2e199162922c6d553
Kevin Quing <KevinQ@iqmetrix.com>
2016-06-11 20:58:47 +0800

    -Added common project for common functions. But for now, just automapper caller to enable Dependency Injection for unit testing of api which uses automapper -Removed unused using references in officeApiService -Added unit test for office-getasync. Will be updated for more in the future

--- BeerTapV2.UnitTest/OfficeApiServiceTest.cs
+++ BeerTapV2.UnitTest/OfficeApiServiceTest.cs
@@ -5,9 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using BeerTapV2.ApiServices;
 using BeerTapV2.ApiServices.ApiServiceInterface;
+using BeerTapV2.DTO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IQ.Platform.Framework.WebApi;
 using BeerTapV2.Repository;
+using Moq;
 
 namespace BeerTapV2.UnitTest
 {
@@ -15,31 +17,26 @@ namespace BeerTapV2.UnitTest
     [TestClass]
     public class OfficeApiServiceTest
     {
-        Moq.Mock<IRequestContext> _MockRequestContext;
-        IRequestContext _RequestContext;
-        Moq.Mock<IBeerTapRepository> _MockRepo;
-        IBeerTapRepository _Repo;
-        
-        IOfficeApiService _api;
-
-        public OfficeApiServiceTest()
-        {
-            _MockRequestContext = new Moq.Mock<IRequestContext>();
-            _RequestContext = _MockRequestContext.Object;
-            _MockRepo = new Moq.Mock<IBeerTapRepository>();
-            _Repo = _MockRepo.Object;
-
-            _api = new OfficeApiService(_Repo);
-        }
+        private IOfficeApiService _api;
 
         [TestMethod]
         public void GetAsync_OfficeWithIdExists_CorrespondingPropertiesShouldHaveSameValues()
         {
-            var idPassed = 1;
-            _api.GetAsync(idPassed, _RequestContext, new System.Threading.CancellationToken());
-
-            _MockRepo.Verify(t => t.FindOffice(idPassed));
+            //Arrange
+            Mock<IRequestContext> mockContext = new Mock<IRequestContext>();
+            Mock<IBeerTapRepository> mockRepo = new Mock<IBeerTapRepository>();
+            _api = new OfficeApiService(mockRepo.Object);
             
+            var officeResDto = new OfficeResourceDto { Id = 1, Name = "Vancouver" };
+            mockRepo.Setup(x => x.FindOffice(It.IsAny<int>())).Returns(
+                () => new OfficeResourceDto { Id = officeResDto.Id, Name = officeResDto.Name });
+
+            //Act
+            var GetAsyncReturn = _api.GetAsync(officeResDto.Id, mockContext.Object, new System.Threading.CancellationToken());
+
+            //Assert
+            mockRepo.Verify(t => t.FindOffice(officeResDto.Id));
+            //TODO: test that return(GetAsyncReturn) data is same as the test request data
 
         }
     }
---
+++ BeerTapV2/BeerTapV2.Common/AutoMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BeerTapV2.Common
+{
+    public interface IAutoMap
+    {
+        TClassTo Map< TClassFrom, TClassTo>(TClassFrom src);
+    }
+
+    public class AutoMap: IAutoMap
+    {
+        public TClassTo Map<TClassFrom, TClassTo>(TClassFrom src)
+        {
+            return AutoMapper.Mapper.Map<TClassFrom, TClassTo>(src);
+        }
+    }
+
+    
+}
